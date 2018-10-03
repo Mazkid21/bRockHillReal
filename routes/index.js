cf11e2970e572b2236ed44872d2c8f8b8245749a
Mazkid21 <zmaraziti@yahoo.com>
2018-10-03 00:04:18 -0600

    one api call loading on page load, other one is working on button click, need to add form form for search options, then make it look pretty and add the link for next 10 listings to appear

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,15 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
-    Content.find(function(err, content) {
-      res.render('index', { title: 'Node Project', contents: content });
+router.get('/', function (req, res, next) {
+  Content.find(function (err, content) {
+    res.render('index', {
+      title: 'Node Project',
+      contents: content
+    });
   });
 });
+
 
 
 module.exports = router;
--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
   // API HEAD INFO WITH KEY AND SUCH
-  console.log("hiiii");
+  console.log("hiiii from server");
   var options = {
     method: 'GET',
     url: 'https://sparkapi.com/Reso/OData/Property',
@@ -20,21 +20,102 @@ router.get('/', (req, res) => {
       authorization: process.env.API_KEY
     }
   };
-  //ACTUALL API CALL
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-
-    let data = JSON.parse(body);
-    let propertyData = data.value;
-    console.log(data);
-
-    // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+  onLoadrequests(options, function (data) {
+    console.log(data + " this is data from back end");
+    var propertyData = data;
     res.render('listing', {
-      property: propertyData
+      property: data
     });
+    // res.send({
+    //   property: data
+    // });
   });
-  //RENDERS home.hbs PASSES PROPERTY VARIABLE(object) TO HANDLEBARS AND CAN BE CALLED USING IMAGES TAG
 
 });
 
+//Onload API CALL
+function onLoadrequests(options, callback) {
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    var resultsArray = [];
+    body = JSON.parse(body);
+
+    // logic used to compare search results with the input from user
+    if (!body.value) {
+      results = "No results found. Try again.";
+      callback(results);
+    } else {
+      propertyData = body.value;
+
+      // for (var i = 0; i < results.length; i++) {
+      //   console.log(i + "this is the index nimbers ");
+      //   resultsArray.push({
+      //     directions: results
+      //   });
+      // };
+    };
+    // pass back the results to client side
+    callback(propertyData);
+  });
+};
+
+router.get('/search', (req, res) => {
+  // API HEAD INFO WITH KEY AND SUCH
+  console.log("hiiii from server");
+  var options = {
+    method: 'GET',
+    url: 'https://sparkapi.com/Reso/OData/Property',
+    qs: {
+      '$filter': 'City eq \'Aspen\' and MlsStatus eq \'Active\'',
+      '$expand': 'CustomFields,Media',
+      '$orderby': 'ListPrice desc',
+      '$count': 'true'
+    },
+    headers: {
+      'x-sparkapi-user-agent': 'BrittanieRockhill',
+      accept: 'application/json',
+      authorization: process.env.API_KEY
+    }
+  };
+  requests(options, function (data) {
+    console.log(data + " this is data from back end");
+    var propertyData = data;
+    // res.render('listing', {
+    //   property: data
+    // });
+    res.send({
+      property: data
+    });
+  });
+
+});
+
+
+//ACTUALL API CALL
+function requests(options, callback) {
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    var resultsArray = [];
+    body = JSON.parse(body);
+
+    // logic used to compare search results with the input from user
+    if (!body.value) {
+      results = "No results found. Try again.";
+      callback(results);
+    } else {
+      propertyData = body.value;
+
+      // for (var i = 0; i < results.length; i++) {
+      //   console.log(i + "this is the index nimbers ");
+      //   resultsArray.push({
+      //     directions: results
+      //   });
+      // };
+    };
+    // pass back the results to client side
+    callback(propertyData);
+  });
+};
 module.exports = router;

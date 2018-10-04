dcec35cbabdf30be8a2c0ee82d5e8c5d54b9e544
Mazkid21 <zmaraziti@yahoo.com>
2018-10-03 22:03:16 -0600

    rendering new page on click, api call just not returning any data

---
+++ public/js/listingRendered.js
@@ -0,0 +1,37 @@
+$(function () {
+    // var source = $("#search-results").append();
+    // var dataTemplate = Handlebars.compile(source);
+    // $results = $('#results')
+
+    // var template = document.getElementById('cat-list-template').innerHTML;
+    // var renderCats = Handlebars.compile(template);
+    $('#search').on('click', function (e) {
+
+        console.log('HIIII');
+        var idimg = $(this).attr('id');
+        // $.post('/listing', function (data) {
+        //     console.log("pooop2" + JSON.stringify(data[0].directions));
+        var propertyData = {}
+
+        // });
+        $.ajax({
+            method: 'get',
+            dataType: "text",
+            url: '/listing/search',
+            success: function (data) {
+                console.log('poooooooop' + data);
+                var placeHolder = $("#cat-list-template");
+                var test = data[0].Zoning;
+                var image = "you suckkk";
+                var source = $("#image-template").html();
+                var template = Handlebars.compile(source);
+                var html = template(image);
+                console.log(html);
+                placeHolder.append(html);
+
+
+            }
+        });
+    });
+
+});
--- routes/listing.js
+++ routes/listing.js
@@ -4,32 +4,34 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
   // API HEAD INFO WITH KEY AND SUCH
-  console.log("hiiii from server");
-  var options = {
-    method: 'GET',
-    url: 'https://sparkapi.com/Reso/OData/Property',
-    qs: {
-      '$filter': 'City eq \'Aspen\' and MlsStatus eq \'Active\'',
-      '$expand': 'CustomFields,Media',
-      '$orderby': 'ListPrice desc',
-      '$count': 'true'
-    },
-    headers: {
-      'x-sparkapi-user-agent': 'BrittanieRockhill',
-      accept: 'application/json',
-      authorization: process.env.API_KEY
-    }
-  };
-  onLoadrequests(options, function (data) {
-    console.log(data + " this is data from back end");
-    var propertyData = data;
-    res.render('listing', {
-      property: data
-    });
-    // res.send({
-    //   property: data
-    // });
-  });
+  // console.log("hiiii from server");
+  // var options = {
+  //   method: 'GET',
+  //   url: 'https://sparkapi.com/Reso/OData/Property',
+  //   qs: {
+  //     '$filter': 'City eq \'Aspen\' and MlsStatus eq \'Active\'',
+  //     '$expand': 'CustomFields,Media',
+  //     '$orderby': 'ListPrice desc',
+  //     '$count': 'true'
+  //   },
+  //   headers: {
+  //     'x-sparkapi-user-agent': 'BrittanieRockhill',
+  //     accept: 'application/json',
+  //     authorization: process.env.API_KEY
+  //   }
+  // };
+  // onLoadrequests(options, function (data) {
+  //   console.log(data + " this is data from back end");
+  //   var propertyData = data;
+  //   res.render('listing', {
+  //     property: data
+  //   });
+  //   // res.send({
+  //   //   property: data
+  //   // });
+  // });
+
+  res.render('listing');
 
 });
 
@@ -102,12 +104,13 @@ function requestsRefinded(options, callback) {
 
   request(options, function (error, response, body) {
     console.log(JSON.stringify(options) + "this is the option from requestes()");
+    console.log(body + ": this is the body");
     if (error) throw new Error(error);
     var resultsArray = [];
     body = JSON.parse(body);
 
     // logic used to compare search results with the input from user
-    if (!body.value) {
+    if (!body) {
       results = "No results found. Try again.";
       callback(results);
     } else {

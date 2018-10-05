92f62e9db5b0ab1cfed033e1c8859c8c3e933ecb
Mazkid21 <zmaraziti@yahoo.com>
2018-10-04 22:48:51 -0600

    search is working, just need to add citys and if function to check if there are paramaters for api call or not

--- routes/listing.js
+++ routes/listing.js
@@ -2,54 +2,6 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-
-// router.post('/poop', function (req, res) {
-//   response = {
-//     minPrice: req.body.minPrice,
-//     maxPrice: req.body.maxPrice,
-//     numBed: req.body.numBed,
-//     numBath: req.body.numBath
-//   };
-
-//   var minPriceURLstring = 'and ListPrice ge ' + response.minPrice;
-//   ///IF STATMENT HERE THAT CHECKS IF THE RESPONSE DATA IS THERE
-//   console.log(minPriceURLstring);
-//   console.log(response.minPrice + ": this is the min price that the api call is getting as a header");
-//   var options = {
-//     method: 'GET',
-//     url: 'https://sparkapi.com/Reso/OData/Property',
-//     qs: {
-//       '$filter': 'MlsStatus eq \'Active\' and ListPrice le ' + response.maxPrice + 'and ListPrice ge ' + response.minPrice + 'and BedroomsTotal ge ' + response.numBed + 'and BathroomsTotalInteger ge ' + response.numBath + 'and Zoning eq \'Residential\'',
-//       '$expand': 'CustomFields,Media',
-//       '$orderby': 'ListPrice desc',
-//       '$count': 'true'
-//     },
-//     headers: {
-//       'x-sparkapi-user-agent': 'BrittanieRockhill',
-//       accept: 'application/json',
-//       authorization: process.env.API_KEY
-//     }
-//   };
-//   requests(options, function (data) {
-//     console.log(data + " this is data from back end");
-//     var propertyData = data;
-//     res.render('listingRendered', {
-//       property: data
-//     });
-//   });
-
-//   //this line is optional and will print the response on the command prompt
-//   //It's useful so that we know what infomration is being transferred
-//   //using the server
-//   console.log(response);
-
-//   //convert the response in JSON format (working sending the response data tooo listingRendered)
-//   // res.render('listingRendered', {
-//   //   property: response
-//   // })
-// });
-
-
 router.get('/', (req, res) => {
   // API HEAD INFO WITH KEY AND SUCH
   console.log("hiiii from server");
@@ -108,36 +60,6 @@ function onLoadrequests(options, callback) {
   });
 };
 
-// router.get('/search', (req, res) => {
-//   // API HEAD INFO WITH KEY AND SUCH
-//   console.log("hiiii from server");
-//   var options = {
-//     method: 'GET',
-//     url: 'https://sparkapi.com/Reso/OData/Property',
-//     qs: {
-//       '$filter': 'City eq \'Aspen\' and MlsStatus eq \'Active\'',
-//       '$expand': 'CustomFields,Media',
-//       '$orderby': 'ListPrice desc',
-//       '$count': 'true'
-//     },
-//     headers: {
-//       'x-sparkapi-user-agent': 'BrittanieRockhill',
-//       accept: 'application/json',
-//       authorization: process.env.API_KEY
-//     }
-//   };
-//   requests(options, function (data) {
-//     console.log(data + " this is data from back end");
-//     var propertyData = data;
-//     // res.render('listing', {
-//     //   property: data
-//     // });
-//     res.send({
-//       property: data
-//     });
-//   });
-
-// });
 
 
 //ACTUALL API CALL

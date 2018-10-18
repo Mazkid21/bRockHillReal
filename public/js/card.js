be74ee400c7df530297d236efed7c7b8d340217e
Mazkid21 <zmaraziti@yahoo.com>
2018-10-18 16:28:48 -0600

    dont use this commit it suckssss

---
+++ public/js/card.js
@@ -0,0 +1,19 @@
+// $('.mySlider').MxSlider({
+//     autoPlay: true,
+//     dots: true,
+//     timeSlide: 500,
+//     // display navigation arrows
+//     nav: true,
+
+//     // display pagination dots
+//     dots: false,
+
+//     // animation speed
+//     timeSlide: 1000,
+
+//     // autoplay
+//     autoPlay: false,
+
+//     // autoplay interval
+//     autoPlaySpeed: 5000
+// });
